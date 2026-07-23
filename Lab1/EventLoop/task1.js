5bc3bde8915656350475b4f60d1e88b07c81f0df
Dipanshu Roy <roydipanshu02@gmail.com>
2026-07-23 15:36:27 +0530

    task1

--- Lab1/EventLoop/task1.js
+++ Lab1/EventLoop/task1.js
@@ -1,22 +1,36 @@
 const f1 = () => {
-  console.log("f1");
+  console.log("f1 start");
+        f2();
+        console.log("f1 running");
+        console.log("f1 end");
 };
 
 const f2 = () => {
-  console.log("f2");
+   console.log("f2 start");
+        f3();
+        console.log("f2 running");
+        console.log("f2 end");
 };
 
 const f3 = () => {
-  console.log("f3");
+  console.log("f3 start");
+        console.log("f3 running");
+        console.log("f3 stops");
 };
 
 function main() {
-  console.log("main");
-  f1();
-  f2();
-  f3();
-  console.log("end main");
+    console.log("Main starts");
+    f1(); // synchronous Call
+    console.log("Main running");
+    console.log("Main ends");
+
 }
 
-main ();
-
+main();
+// javascript is synchronous and single threaded
+// to overcome single time functiong we use Asynchronous Calling we use event loop to manage the call stack
+// Asynchronpus calls using tomers
+// 1 . set timeout
+// 2. set immediate
+// 3. process.next tick
+// 4. set interval
